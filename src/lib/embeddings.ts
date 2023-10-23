d76c28cbcc1381acf86a77a16732dc2130b1f317
jop <springkraf>
2023-10-23 13:12:20 +0700

    wip

--- src/lib/embeddings.ts
+++ src/lib/embeddings.ts
@@ -9,8 +9,11 @@ const openai = new OpenAIApi(config);
 export async function getEmbeddings(text: string) {
   try {
     const response = await openai.createEmbedding({
-      model: "text-embedding-v1",
+      model: "text-embedding-ada-002",
+      input: text.replace(/\n/g, " "),
     });
+    const result = await response.json();
+    return result.data[0].embedding as number[];
   } catch (error) {
     console.error("error calling openai embeddings api", error);
     throw error;
